bd3e863bc1ca9447fbf35d50036dc04866965d92
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-05-21 20:04:17 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ menu_list_array = ["Chiken Tandoori Pizza",
                       menu_list_array.sort;
                       htmldata="<section class='cards'>";
                       for(var i=0; i<menu_list_array.length;i++){
-                          htmldata=htmldata+'<div class="card">' + '<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>';
+                          htmldata=htmldata+'<div class="card">' + '<img src="istockphoto-1083487948-612x612.jpg"/>' + menu_list_array[i] + '</div>';
                       }
                       htmldata=htmldata + "</section>";
                       document.getElementById("display_addedmenu").innerHTML = htmldata;
